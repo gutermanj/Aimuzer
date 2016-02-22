edf363a21d177f5a7a1b6e48733d537e003a9db2
Julian Guterman <gutermanj@gmail.com>
2016-02-22 18:03:13 -0500

    Fixed the fixed side bar, changed some colors of things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,6 +33,28 @@ $(document).on('ready', function () {
 		getFollowing(user_id);
 	});
 
+	
+var fixmeTop = $('.overeverything').offset().top;       // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+	
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop - 50) {           // apply position: fixed if you
+        $('.overeverything').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '6%',
+            width: '25.3%'
+        });
+    } else {                                   // apply position: static
+        $('.overeverything').css({                      // if you scroll above it
+            position: 'static',
+            width: '100%'
+        });
+    }
+
+});
+
 }); // doc ready
 
 function getFollowers(user_id) {
