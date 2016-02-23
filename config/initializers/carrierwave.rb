e4db8b0031465c8614a082c94885e76543f6cc13
Julian Guterman <gutermanj@gmail.com>
2016-02-23 14:47:37 -0500

    Fixed AWS key issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAILUQBOMOARKL5FIQ',                        # required
-    :aws_secret_access_key  => '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',                     # required
+    :aws_access_key_id      => ENV["AWS_KEY_ID"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY_ID"],                     # required
     # :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
      :host                   => 's3.amazonaws.com',             # optional, defaults to nil
     # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
